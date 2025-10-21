aa7730565a736f7b21a9d67c6ed2c42149ff7f43
kurushiba <kurushiba55555@gmail.com>
2025-10-21 09:35:10 +0900

    不要なファイル削除

--- src/lib/api/interceptors/request.ts
+++
@@ -1,9 +0,0 @@
-import type { InternalAxiosRequestConfig } from 'axios';
-
-export const addAuthorizationHeader = (config: InternalAxiosRequestConfig) => {
-  const token = localStorage.getItem('token');
-  if (token == null) return config;
-
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
-};
